4314652b28ef10ffa894fa9dae5949de79994f95
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-29 15:00:37 -0400

    Init commit

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+// console.log("Hello World!\n==========\n");
+console.log()
+//   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+// );
 
 // Exercise 1
 
+// const firstName = `Mike`;
+let lastName;
+var age;
+
+console.log("Exercise 1:\n===========\n");
+console.log("lastName:", lastName); // This will log 'undefined'
+console.log("age:", age); // This will log 'undefined'
+
 // Exercise 2
 
+const firstName = "John";
+lastName = "Doe";
+age = 29;
+
+console.log("\nExercise 2:\n");
+console.log(`My Name is ${firstName} ${lastName} I'm ${age} Years old.`)
+
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log("\nExercise 3:\n===========\n");
+console.log(`${language} was created in ${createdYear}\n is it case sensitive? ${isCaseSensitive}.`)
+
+// Exercise 4
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log("\nExercise 4:\n===========\n");
+console.log("how much is it?:", price);
+console.log("Might be On Sale:", isOnSale);
+console.log("what is the sale Percentage?", salePercentage);
+console.log("How many in stock?", stock);
+console.log("Have any other Stock?", inStock);
+console.log("what Size?", selectedSize);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456 ;
+hasRead = true;
+
+console.log("\nExercise 5:\n===========\n");
+console.log(`The Book ${title} by ${author} has ${pageCount} pages my bookmark is at ${bookmark}. Did read it? ${hasRead}.`);
